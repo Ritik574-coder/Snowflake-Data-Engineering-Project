2d401c1c6f52d98fd92e0562fcb5a53b20befc61
ritik574-coder <ritik74820@gmail.com>
2026-08-04 23:58:50 +0530

    addign python code for analysis the data that are in csv format

--- Full-Load-ELT-Data-Warehouse/loading_data/ddl_table.sql
+++ Full-Load-ELT-Data-Warehouse/loading_data/ddl_table.sql
@@ -1,4 +1,4 @@
--- sefty check before creating ddl script 
+-- sefty check before running ddl script 
 DECLARE
     WRONG_DATABASE EXCEPTION (
         -20001,
@@ -12,3 +12,14 @@ END;
 
 -- switch database to RitskySnow 
 USE DATABASE RITSKYSNOW ;
+
+
+CREAT OR REPLACE TABLE aircraft(
+    aircraft_id      VARCHAR(100),   
+    tail_number      VARCHAR(100), 
+    model_id         VARCHAR(100),       
+    airline_id       VARCHAR(100),   
+    manufacture_year INT         ,    
+    status           VARCHAR(100)
+) ;
+           
