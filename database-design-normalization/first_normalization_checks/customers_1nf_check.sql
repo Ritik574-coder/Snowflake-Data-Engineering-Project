ac549533d446e9f21b944c44b2bb3e46894a0ffc
ritik574-coder <ritik74820@gmail.com>
2026-08-24 16:42:40 +0530

    adding new script for snowflake that finding and counting those row they validate 1NF fule

--- database-design-normalization/first_normalization_checks/customers_1nf_check.sql
+++ database-design-normalization/first_normalization_checks/customers_1nf_check.sql
@@ -1,10 +1,14 @@
 -- ===========================================================================================
 -- =================== FIRST NORMALIZATION CHECK IN CUSTOMERS DATASET ========================
 -- ===========================================================================================
+-- switch corrent databse and schema 
+USE DATABASE NORMALIZE_DW ;
+USE SCHEMA STAGING ; 
 -- Checking for customer_id values that potentially violate 1NF
 -- by identifying rows where a single customer_id field may contain multiple values.
+
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(CUSTOMER_ID, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -12,7 +16,7 @@ FROM customers;
 -- Checking for customer_name values that potentially violate 1NF
 -- by identifying rows where a single customer_name field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(CUSTOMER_NAME, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -20,7 +24,7 @@ FROM customers;
 -- Checking for email values that potentially violate 1NF
 -- by identifying rows where a single email field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(EMAIL, '.*[,|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -28,7 +32,7 @@ FROM customers;
 -- Checking for phone values that potentially violate 1NF
 -- by identifying rows where a single phone field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(PHONE, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -36,7 +40,7 @@ FROM customers;
 -- Checking for city values that potentially violate 1NF
 -- by identifying rows where a single city field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(CITY, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -44,7 +48,7 @@ FROM customers;
 -- Checking for state values that potentially violate 1NF
 -- by identifying rows where a single state field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(STATE, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -52,7 +56,7 @@ FROM customers;
 -- Checking for country values that potentially violate 1NF
 -- by identifying rows where a single country field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
+    COUNT(*) AS total_row,
     COUNT_IF(REGEXP_LIKE(COUNTRY, '.*[,.|*:;].*')) AS suspicious_row
 FROM customers;
 
@@ -60,6 +64,6 @@ FROM customers;
 -- Checking for signup_date values that potentially violate 1NF
 -- by identifying rows where a single signup_date field may contain multiple values.
 SELECT 
-    COUNT(*) AS data_count,
-    COUNT_IF(REGEXP_LIKE(SIGNUP_DATE, '.*[,.|*:;].*')) AS suspicious_row
+    COUNT(*) AS total_row,
+    COUNT_IF(REGEXP_LIKE(SIGNUP_DATE, '.*[,|*:;].*')) AS suspicious_row
 FROM customers;
