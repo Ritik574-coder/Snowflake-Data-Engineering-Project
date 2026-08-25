5a1f93664538b891b53bc9f8e9870d27e3ee4255
ritik574-coder <ritik74820@gmail.com>
2026-08-25 09:44:06 +0530

    adding new string in file named store_1nf_check this query counting those row they validate first atomic prienciple

--- database-design-normalization/first_normalization_checks/stores_1nf_check.sql
+++ database-design-normalization/first_normalization_checks/stores_1nf_check.sql
@@ -6,3 +6,73 @@ USE DATABASE NORMALIZE_DW ;
 
 -- Switch to corrent schema 
 USE SCHEMA STAGING ; 
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  STORE_ID
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            STORE_ID, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  STORE_NAME
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            STORE_NAME, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  CITY
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            CITY, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  STATE
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            STATE, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  COUNTRY
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            COUNTRY, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  MANAGER_ID
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            MANAGER_ID, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name  MANAGER_NAME
+SELECT 
+    COUNT(*) AS total_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            MANAGER_NAME, '.*[!,|:;_*].*'
+        )
+    ) AS suspicious_count
+FROM stores ;
