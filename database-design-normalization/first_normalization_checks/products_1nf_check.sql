bb569ae860619c73ccd09a433efa40900788ee82
ritik574-coder <ritik74820@gmail.com>
2026-08-25 09:10:16 +0530

    adding new script file named product_1nf_check this script check first atomic value in product table

---
+++ database-design-normalization/first_normalization_checks/products_1nf_check.sql
@@ -0,0 +1,71 @@
+-- ===========================================================================================
+-- ==================== FIRST NORMALIZATION CHECK IN PRODUCTS DATASET ========================
+-- ===========================================================================================
+-- Switch to correct database
+USE DATABASE NORMALIZE_DW ;
+
+-- Switch to correct schema 
+USE SCHEMA STAGING ; 
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name PRODUCT_ID
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            PRODUCT_ID, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name PRODUCT_NAME
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            PRODUCT_NAME, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name CATEGORY_ID
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            CATEGORY_ID, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name CATEGORY_NAME
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            CATEGORY_NAME, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name SUPPLIER_ID
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            SUPPLIER_ID, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name SUPPLIER_NAME
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            SUPPLIER_NAME, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
+
+-- checking suspicious row and checking the every colimn contain single atomic valiue and these are not represent group in column name UNIT_PRICE
+SELECT 
+    COUNT(*) AS totla_row,
+    COUNT_IF(
+        REGEXP_LIKE(
+            UNIT_PRICE, '.*[!|,_:;].*')
+    ) AS suspicious_count
+FROM products ;
