928c1648fb759ec842c5fde6352eab0a722e5fcd
ritik574-coder <ritik74820@gmail.com>
2026-08-27 15:40:38 +0530

    feat: add normalized dimension schema and data loading

--- database-design-normalization/third_normalization/customers_3nf.sql
+++ database-design-normalization/third_normalization/customers_3nf.sql
@@ -25,41 +25,38 @@ FROM customers
 GROUP BY state
 HAVING COUNT(DISTINCT country) > 1;
 
-
 --##########################################################################################
 --############################## CREATING DDL SCRIPT  ######################################
 --##########################################################################################
 -- Switch to correct database
 USE DATABASE NORMALIZE_DW;
--- Switch to the DIMENSIONS schema
-USE SCHEMA DIMENSIONS;
 
+-- Switch to the DIMENSIONS schema
+USE SCHEMA dimensions;
 
 -- Create the country DIMENSIONS table country_id is the surrogate primary key
 -- country must be unique and cannot be NULL
-CREATE TABLE DIMENSIONS.dim_countries (
-    country_id NUMBER AUTOINCREMENT PRIMARY KEY,
-    country VARCHAR(60) UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS dimensions.dim_countries (
+    country_id  NUMBER AUTOINCREMENT PRIMARY KEY,
+    country     VARCHAR(60) UNIQUE NOT NULL
 );
-
 
 -- Create the state DIMENSIONS table Each state is linked to its corresponding country
 -- country_id acts as a foreign key to the country DIMENSIONS
-CREATE TABLE DIMENSIONS.dim_states (
-    state_id NUMBER AUTOINCREMENT PRIMARY KEY,
-    state VARCHAR(60) UNIQUE NOT NULL,
-    country_id INTEGER NOT NULL,
+CREATE TABLE IF NOT EXISTS dimensions.dim_states (
+    state_id    NUMBER AUTOINCREMENT PRIMARY KEY,
+    state       VARCHAR(60) UNIQUE NOT NULL,
+    country_id  INTEGER NOT NULL,
 
     FOREIGN KEY (country_id)
         REFERENCES dim_countries(country_id)
 );
 
-
 -- Create the city DIMENSIONS table Each city is linked to its corresponding state
 -- state_id acts as a foreign key to the state DIMENSIONS
-CREATE TABLE DIMENSIONS.dim_cities (
-    city_id NUMBER AUTOINCREMENT PRIMARY KEY,
-    city VARCHAR(60) UNIQUE NOT NULL,
+CREATE TABLE IF NOT EXISTS dimensions.dim_cities (
+    city_id  NUMBER AUTOINCREMENT PRIMARY KEY,
+    city     VARCHAR(60) UNIQUE NOT NULL,
     state_id INTEGER NOT NULL,
 
     FOREIGN KEY (state_id)
@@ -69,15 +66,74 @@ CREATE TABLE DIMENSIONS.dim_cities (
 
 -- Create the customer DIMENSIONS table Each customer is linked to a city through city_id
 -- customer_id is the primary key email must be unique and cannot be NULL
-CREATE TABLE DIMENSIONS.dim_customers (
-    customer_id VARCHAR(10) PRIMARY KEY,
-    customer_name VARCHAR(120) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    phone VARCHAR(30),
-    city_id INTEGER NOT NULL,
-    signup_date DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS dimensions.dim_customers (
+    customer_id     VARCHAR(10) PRIMARY KEY,
+    customer_name   VARCHAR(120) NOT NULL,
+    email           VARCHAR(150) UNIQUE NOT NULL,
+    phone           VARCHAR(30),
+    city_id         INTEGER NOT NULL,
+    signup_date     DATE NOT NULL,
 
     FOREIGN KEY (city_id)
         REFERENCES dim_cities(city_id)
 );
 
+--##########################################################################################
+--################################### DATA INSERTING  ######################################
+--##########################################################################################
+
+INSERT INTO dimensions.dim_countries(
+    country
+)
+SELECT
+    country
+FROM staging.customers 
+GROUP BY country; 
+
+INSERT INTO dimensions.dim_states(
+    state,
+    country_id
+)
+SELECT 
+    c.state,
+    dc.country_id
+FROM staging.customers as c 
+INNER JOIN dimensions.dim_countries AS dc
+ON c.country = dc.country
+GROUP BY
+    c.state,
+    dc.country_id ;
+
+
+INSERT INTO dimensions.dim_cities(
+    city,
+    state_id
+)
+SELECT 
+    c.city,
+    s.state_id
+FROM staging.customers as c 
+INNER JOIN dimensions.dim_states as s 
+ON s.state = c.state
+GROUP BY
+    c.city,
+    s.state_id ;
+
+INSERT INTO dimensions.dim_customers(
+    customer_id,
+    customer_name,
+    email,
+    phone,
+    city_id,
+    signup_date
+)
+SELECT 
+    c.customer_id,
+    c.customer_name,
+    c.email,
+    c.phone,
+    dc.city_id,
+    c.signup_date
+FROM staging.customers as c
+INNER JOIN dimensions.dim_cities as dc 
+ON c.city = dc.city ;
